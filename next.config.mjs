3eb2aedc3e0a465b64acf0a1f9f22c89ea6a6d06
pm6482545-lang <pm6482545@gmail.com>
2026-09-24 12:22:13 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
+  experimental: {
+    serverActions: { bodySizeLimit: '4mb' },
+  },
 }
 
 export default nextConfig
